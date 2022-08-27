2ad41587990d2e38e001e9fbbe047d4a5da22de2
Tuukka <mail@tuukkaorm.io>
2022-08-28 00:33:44 +0300

    teht 10.12

--- App.js
+++ App.js
@@ -2,12 +2,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
+import Constants from 'expo-constants';
 import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
 
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.manifest);
+
   return (
     <>
       <NativeRouter>
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const httpLink = createHttpLink({
   // Replace the IP address part with your own IP address!
-  uri: 'http://192.168.0.112:4000/graphql',
+  uri: Constants.manifest.extra.uri,
 });
 
 const createApolloClient = () => {
