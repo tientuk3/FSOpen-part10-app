0ae2770c8bbbf36bcac49f32050b282efc162ed1
Tuukka <mail@tuukkaorm.io>
2022-08-28 18:31:20 +0300

    välikonffausta

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,12 +2,9 @@ import { FlatList } from 'react-native';
 import RepositoryItem from './RepositoryItem'
 import useRepositories from '../hooks/useRepositories';
 
-const RepositoryList = () => {
-  const { repositories } = useRepositories();
-
-  // Get the nodes from the edges array
+export const RepositoryListContainer = ({ repositories }) => {
   const repositoryNodes = repositories
-    ? repositories.edges.map(edge => edge.node)
+    ? repositories.edges.map((edge) => edge.node)
     : [];
 
   return (
@@ -17,6 +14,13 @@ const RepositoryList = () => {
       renderItem={RepositoryItem}
     />
   );
+};
+
+
+const RepositoryList = () => {
+  const { repositories } = useRepositories();
+
+  return <RepositoryListContainer repositories={repositories} />;
 };
 
 export default RepositoryList;
